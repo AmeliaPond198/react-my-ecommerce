dea6ba452bf080f1d46cf62d5858a87c0d3efc92
AmeliaPond198 <molinari.alice@virgilio.it>
2022-12-31 12:14:00 +0100

    add observer listener to check di volta in volta the authentication state of the user

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,7 +5,9 @@ import {
   signInWithPopup, 
   GoogleAuthProvider, 
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
  } from 'firebase/auth';
 import {
   getFirestore,
@@ -85,6 +87,14 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password)
 
+};
+
+export const signOutUser = async () => {
+  return await signOut(auth);
+};
+
+export const onAuthStateChangedListener = (callback) => {
+  return onAuthStateChanged(auth, callback)
 };
 
 
